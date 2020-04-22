cadda74abc7a971256d26f322630bf6941a8eb84
dashoook <dashkav2007@gmail.com>
2020-04-22 14:56:31 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace _2._1_LABA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Введiть номер квартири:");
+            a = double.Parse(Console.ReadLine());
+            while (a > 50 || a < 1)
+            {
+                Console.WriteLine("Введено неправильний номер квартити. Введiть номер квартири:");
+                a = double.Parse(Console.ReadLine());
+            }
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Ви знаходитесь на 1 поверсi.");
+                    break;
+                case 2:
+                    Console.WriteLine("Ви знаходитесь на 1 поверсi.");
+                    break;
+                case 3:
+                    Console.WriteLine("Ви знаходитесь на 1 поверсi.");
+                    break;
+                case 4:
+                    Console.WriteLine("Ви знаходитесь на 1 поверсi.");
+                    break;
+                case 5:
+                    Console.WriteLine("Ви знаходитесь на 1 поверсi.");
+                    break;
+                case 6:
+                    Console.WriteLine("Ви знаходитесь на 2 поверсi.");
+                    break;
+                case 7:
+                    Console.WriteLine("Ви знаходитесь на 2 поверсi.");
+                    break;
+                case 8:
+                    Console.WriteLine("Ви знаходитесь на 2 поверсi.");
+                    break;
+                case 9:
+                    Console.WriteLine("Ви знаходитесь на 2 поверсi.");
+                    break;
+                case 10:
+                    Console.WriteLine("Ви знаходитесь на 2 поверсi.");
+                    break;
+                case 11:
+                    Console.WriteLine("Ви знаходитесь на 3 поверсi.");
+                    break;
+                case 12:
+                    Console.WriteLine("Ви знаходитесь на 3 поверсi.");
+                    break;
+                case 13:
+                    Console.WriteLine("Ви знаходитесь на 3 поверсi.");
+                    break;
+                case 14:
+                    Console.WriteLine("Ви знаходитесь на 3 поверсi.");
+                    break;
+                case 15:
+                    Console.WriteLine("Ви знаходитесь на 3 поверсi.");
+                    break;
+                case 16:
+                    Console.WriteLine("Ви знаходитесь на 4 поверсi.");
+                    break;
+                case 17:
+                    Console.WriteLine("Ви знаходитесь на 4 поверсi.");
+                    break;
+                case 18:
+                    Console.WriteLine("Ви знаходитесь на 4 поверсi.");
+                    break;
+                case 19:
+                    Console.WriteLine("Ви знаходитесь на 4 поверсi.");
+                    break;
+                case 20:
+                    Console.WriteLine("Ви знаходитесь на 4 поверсi.");
+                    break;
+                case 21:
+                    Console.WriteLine("Ви знаходитесь на 5 поверсi.");
+                    break;
+                case 22:
+                    Console.WriteLine("Ви знаходитесь на 5 поверсi.");
+                    break;
+                case 23:
+                    Console.WriteLine("Ви знаходитесь на 5 поверсi.");
+                    break;
+                case 24:
+                    Console.WriteLine("Ви знаходитесь на 5 поверсi.");
+                    break;
+                case 25:
+                    Console.WriteLine("Ви знаходитесь на 5 поверсi.");
+                    break;
+                case 26:
+                    Console.WriteLine("Ви знаходитесь на 6 поверсi.");
+                    break;
+                case 27:
+                    Console.WriteLine("Ви знаходитесь на 6 поверсi.");
+                    break;
+                case 28:
+                    Console.WriteLine("Ви знаходитесь на 6 поверсi.");
+                    break;
+                case 29:
+                    Console.WriteLine("Ви знаходитесь на 6 поверсi.");
+                    break;
+                case 30:
+                    Console.WriteLine("Ви знаходитесь на 6 поверсi.");
+                    break;
+                case 31:
+                    Console.WriteLine("Ви знаходитесь на 7 поверсi.");
+                    break;
+                case 32:
+                    Console.WriteLine("Ви знаходитесь на 7 поверсi.");
+                    break;
+                case 33:
+                    Console.WriteLine("Ви знаходитесь на 7 поверсi.");
+                    break;
+                case 34:
+                    Console.WriteLine("Ви знаходитесь на 7 поверсi.");
+                    break;
+                case 35:
+                    Console.WriteLine("Ви знаходитесь на 7 поверсі.");
+                    break;
+                case 36:
+                    Console.WriteLine("Ви знаходитесь на 8 поверсі.");
+                    break;
+                case 37:
+                    Console.WriteLine("Ви знаходитесь на 8 поверсі.");
+                    break;
+                case 38:
+                    Console.WriteLine("Ви знаходитесь на 8 поверсі.");
+                    break;
+                case 39:
+                    Console.WriteLine("Ви знаходитесь на 8 поверсi.");
+                    break;
+                case 40:
+                    Console.WriteLine("Ви знаходитесь на 8 поверсi.");
+                    break;
+                case 41:
+                    Console.WriteLine("Ви знаходитесь на 9 поверсi.");
+                    break;
+                case 42:
+                    Console.WriteLine("Ви знаходитесь на 9 поверсi.");
+                    break;
+                case 43:
+                    Console.WriteLine("Ви знаходитесь на 9 поверсi.");
+                    break;
+                case 44:
+                    Console.WriteLine("Ви знаходитесь на 9 поверсi.");
+                    break;
+                case 45:
+                    Console.WriteLine("Ви знаходитесь на 9 поверсi.");
+                    break;
+            }
+        }
+    }
+}
